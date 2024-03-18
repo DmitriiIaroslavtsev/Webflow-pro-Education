4b40c3774a619aa3c3a361d0e79659fe18be2ee7
DmitriiIaroslavtsev <yaroslavtsevda@gmail.com>
2024-03-18 17:10:45 +0400

    folder

---
+++ lesson2/app.js
@@ -0,0 +1,10 @@
+console.log('Hello')
+const Name = 'Dmitrii'
+const age = 27;
+const City = 'Saint Petersburg'
+console.log(age)
+console.log(City)
+let myLifeStyle = 'freelance';
+let myAge = 30;
+myAge = 25;
+console.log(myAge);
