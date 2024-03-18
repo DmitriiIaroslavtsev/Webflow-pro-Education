9f7176bcc1310774af308ec7d73cbbb6739261c2
DmitriiIaroslavtsev <yaroslavtsevda@gmail.com>
2024-03-18 17:51:00 +0400

    l3

---
+++ Lesson3/l3.js
@@ -0,0 +1,20 @@
+
+let titleProject = 'Оценка стоиости';
+console.log(titleProject);
+console.log(typeof titleProject);
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+console.log(screensValue);
+let screenPrice = 100;
+console.log(screenPrice);
+console.log(typeof screenPrice);
+let percentage = 20;
+console.log(percentage);
+let fullPrice = 1000;
+let responsive = false;
+if (responsive) {
+    console.log('good');
+} else {
+    console.log('bad');
+}
+console.log(typeof responsive);
+
