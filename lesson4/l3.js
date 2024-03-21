7a7b46b154383bfec6a6926c5b54f7c4440d9d04
DmitriiIaroslavtsev <yaroslavtsevda@gmail.com>
2024-03-21 19:55:15 +0400

    Lesson3

---
+++ lesson4/l3.js
@@ -0,0 +1,46 @@
+
+//let titleProject = 'Оценка стоиости';
+//console.log(titleProject);
+//console.log(typeof titleProject);
+/*let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+console.log(screensValue);
+let screenPrice = 100;
+console.log(screenPrice);
+console.log(typeof screenPrice);
+let percentage = 20;
+console.log(percentage);
+let fullPrice = 1000;
+let responsive = true;
+if (responsive) {
+    console.log('good');
+} else {
+    console.log('bad');
+}
+console.log(typeof responsive);*/
+
+
+let project = prompt('Название проекта?');
+console.log(project);
+let res = project;
+let screensValue = prompt('шаблонные, с уникальным дизайном, с анимациями');
+console.log(screensValue);
+let responsive = confirm ('Нужен ли адаптив');
+let service1 = prompt('Какой сервис Вам нужен?');
+let servicePrice1 = +prompt('СКолько это будет стоить?');
+let service2 = prompt('Какой еще сервис нужен?');
+let servicePrice2 = + prompt ( 'Сколько будет стоить второй сервис?');
+let screenPrice = 1000;
+let servicePercentPrice = (servicePrice1 + servicePrice2 + screenPrice) * 0.15
+servicePercentPrice = Math.round( servicePercentPrice);
+let fullPrice = servicePrice1 + servicePrice2 + screenPrice - servicePercentPrice;
+if (fullPrice > 50000) {
+    console.log('скидка в 10%');
+} else if (fullPrice >20000 && fullPrice <= 50000) {
+    console.log ('Скидка в 5%');
+} else if (fullPrice < 20000 ){ 
+   console.log('Скидки нет'); 
+} else {
+console.log('Что-то пошло не так');
+};
+console.log("Общая стоимость проекта: " + fullPrice);
+console.log("Итоговая сумма за вычетом процента : " + servicePercentPrice);
